97993af11c872bb7c323bef790a0b685c6be8b48
Jatoth venkanna <97695215+jatothvenkanna@users.noreply.github.com>
2022-08-01 22:52:32 +0530

    "Helloworld program"

---
+++ javaproject/src/com/javaproject/demo/coreJava/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.javaproject.demo.coreJava;
+
+import java.sql.SQLOutput;
+
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+
+}
