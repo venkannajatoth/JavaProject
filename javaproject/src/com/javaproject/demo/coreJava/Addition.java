f5a0d9f979bdec19071801871938ab59cc9dd638
Jatoth venkanna <97695215+jatothvenkanna@users.noreply.github.com>
2022-08-01 23:08:17 +0530

    adding addition class to the java project

---
+++ javaproject/src/com/javaproject/demo/coreJava/Addition.java
@@ -0,0 +1,9 @@
+package com.javaproject.demo.coreJava;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        System.out.println(a+b);
+    }
+}
